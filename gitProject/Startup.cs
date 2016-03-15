91dd8a714504d5f96f652b226b4b20e8a9def8b8
Bogdan Stoian <bsmathrex@gmail.com>
2016-03-15 13:13:29 +0200

    add files

---
+++ gitProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gitProject.Startup))]
+namespace gitProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
