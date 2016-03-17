d7a23ed585d94b925e728340a56329825e739de4
Bogdan Stoian <bsmathrex@gmail.com>
2016-03-18 00:37:44 +0200

    fsgsgffgf

--- gitProject/Controllers/HomeController.cs
+++ gitProject/Controllers/HomeController.cs
@@ -10,14 +10,7 @@ using System.Web.Mvc;
     --fete noi alin..context ..am intrat in vorba cu ele, pe strada de ce sa le evit intra in vb cu ele , nu le pot lasa
     pur si simplu sa treaca pe langa mine, ar fi pacat, pot fi niste super fete , daca as bea o bere si apoi as
     merge sa le abordez nu prea mi-ar mai pasa daca as Fii refuzat, de ce s-ar putea sa mi se intample..
-    --buci mari, sani mari..ok cu cat arata mai bine are impact mai mare sexual asupra mea, dar creierul e la fel..depinde
-        ce poate fata din punct de vedere mintal..cat o duce mintea
-
-    social:
-        --> om metrou 1 mai..cum ajunge la unirii
-        --> contact fata ..s-au prins castile mele in parul ei, So am atras atentia..a fost ok..s-a intors putin..am cerut
-        scuze , Nu am mai avut nicio reactie si ia a intors capul si a mers mai departe . chiar daca o fata e 
-        serioasa n
+    --bu
 namespace gitProject.Controllers
 {
     public class HomeController : Controller
