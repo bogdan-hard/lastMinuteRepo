4e4772c2fa617f867b90112a18f97b7eff4db5d3
Bogdan Stoian <bsmathrex@gmail.com>
2016-03-18 00:34:18 +0200

    fsgs

--- gitProject/Controllers/HomeController.cs
+++ gitProject/Controllers/HomeController.cs
@@ -3,7 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+-alg genetici..=> iesi afara, descopera oameni => vei da pe oameni de calitate
+- aprtamentele compartimentate :2,3,4 camere..de ce?
 
+    --frizerie, cursuri PM, administrarea afacerilor..am caracter antreprenorial
+    --fete noi alin..context ..am intrat in vorba cu ele, pe strada de ce sa le evit intra in vb cu ele , nu le pot lasa
+    pur si simplu sa treaca pe langa mine, ar fi pacat, pot fi niste super fete , daca as bea o bere si apoi as
+    merge sa le abordez nu prea mi-ar mai pasa daca as Fii refuzat, de ce s-ar putea sa mi se intample..
+    --buci mari, sani mari..ok cu cat arata mai bine are impact mai mare sexual asupra mea, dar creierul e la fel..depinde
+        ce poate fata din punct de vedere mintal..cat o duce mintea
+
+    social:
+        --> om metrou 1 mai..cum ajunge la unirii
+        --> contact fata ..s-au prins castile mele in parul ei, So am atras atentia..a fost ok..s-a intors putin..am cerut
+        scuze , Nu am mai avut nicio reactie si ia a intors capul si a mers mai departe . chiar daca o fata e 
+        serioasa nu inseamna ca nu ar raspunde pozitiv abordarilor tale
+IMPULS ABORDARE: IRENA, EMILIA, FLORINA
+PRIETEN
+FETE MASTER MANAGEMENT..sunt tanar acum..acum ce pot agata, mai tarziu e mai greu..acum am comoara asta = tineretea
+        ---tipa banca: sa moara franta..=> stilul informal la greu!!!..bazeaza-te pe el, sa nu iti fie teama sa il folosesti
+        sa te lasi inselat de aparente=imagine de relatie oficiala/publica..toata lumea in sinea ei e informala..
+
+        CE A AVUT IRENA LA CV????
 namespace gitProject.Controllers
 {
     public class HomeController : Controller
